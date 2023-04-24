83d473d5ce10dd6b1c5cba4bf0bf91abd983b508
scattamooch <71458730+scattamooch@users.noreply.github.com>
2023-04-24 14:53:10 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+const friends = ["Guadalupe", "Ollie", "Aki"]
+let writeCardsNew = []
+
+function writeCards(friends) {
+    for (let i = 0; i < friends.length; i++) {
+        writeCardsNew.push(`Thank you, ${friends[i]}, for the wonderful surprise gift!`)
+    }
+    return writeCardsNew;
+}
+writeCardsNew
+
+let count = 10;
+
+function countDown(count) {
+    while (count > -1) {
+        console.log(count--);
+    }
+    return count
+}
+countDown(count)
